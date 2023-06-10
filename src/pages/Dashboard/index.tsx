a1f713a0851ac79a455574bd154b52ac946722d8
Sidyvan <sidyvan.dev@gmail.com>
2023-06-10 15:27:10 -0300

    Tela de pesquisa criada

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,35 @@
 import React from 'react';
+import { Title, Form, Repos } from './styles';
+import logo from '../../assets/logo.svg';
+
+import {FiChevronRight} from 'react-icons/fi'
 
 
 export const Dashboard: React.FC = () => {
-  return <h1>Dashboard</h1>;
-}
+  return (
+    <>
+      <img src={logo} alt="GitCollection" />
+      <Title>Catálogo de repositórios</Title>
+      <Form>
+
+        <input placeholder='username/repository_name' />
+        <button type='submit'>Buscar</button>
+      </Form>
+      <Repos>
+          <a href='/repositories'>
+            <img src='https://avatars.githubusercontent.com/u/24855486?v=4' alt='Repositorio' />
+            <div>
+              <strong>repositorio/sidyvan</strong>
+              <p>Repositorio do Sidyvan</p>
+
+            </div>
+            <FiChevronRight size={20}/>
+          </a>
+      </Repos>
+    </>
+   );
+};
+
+
 
 
